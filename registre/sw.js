88555faa8087a5a4c7761b6d8d4b0374a17d38d9
gentinanatacha-byte <gentina.natacha@gmail.com>
2026-09-08 15:17:06 +0200

    ajout du registre

---
+++ registre/sw.js
@@ -0,0 +1,17 @@
+/* Cache hors ligne du Registre. La version change a chaque livraison :
+   le telephone telecharge alors la nouvelle et jette l'ancienne, tout seul. */
+const VERSION = "registre-20260908-1244";
+const FICHIERS = ["./", "./index.html", "./icone-192.png", "./icone-512.png", "./manifest.webmanifest"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(VERSION).then((c) => c.addAll(FICHIERS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", (e) => {
+  e.waitUntil(caches.keys()
+    .then((n) => Promise.all(n.filter((x) => x !== VERSION).map((x) => caches.delete(x))))
+    .then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+});
